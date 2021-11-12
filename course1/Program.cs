781def1302cf814b93e2e8c1e078fab8d24436c9
tl 李 <1473795991@qq.com>
2021-11-12 17:11:01 +0800

    添加项目文件。

---
+++ course1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace course1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            string str = "x i a o m i n g";
+            string[] str1 =str.Split(" ");
+           // Console.WriteLine(str1);
+            string test = String.Join(",", str1);
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+        
+    }
+}
